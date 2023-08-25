03aa40ea6d0abf2dcb70f849990df4f56c12682b
jose caceres <josecaceresmusso@gmail.com>
2023-08-25 11:15:33 -0300

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row ">
+      <div className="flex flex-1 justify-center items-center">
+        {" "}
+        <h1 className="text-white text-center">Hola</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
