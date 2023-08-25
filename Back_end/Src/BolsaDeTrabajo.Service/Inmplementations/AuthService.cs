b0bf0351bfd61b0754b68065d058bb0ebb3aa6c5
FacuQuevedo <qvedo2@hotmail.com>
2023-08-25 12:03:39 -0300

    .

---
+++ Back_end/Src/BolsaDeTrabajo.Service/Inmplementations/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using BolsaDeTrabajo.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolsaDeTrabajo.Service.Inmplementations
+{
+    public class AuthService : IAuthService
+    {
+    }
+}
---
+++ Back_end/Src/BolsaDeTrabjo.Api/Controllers/EnterpriseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BolsaDeTrabjo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnterpriseController : ControllerBase
+    {
+        // GET: api/<EnterpriseController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EnterpriseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EnterpriseController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EnterpriseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EnterpriseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
