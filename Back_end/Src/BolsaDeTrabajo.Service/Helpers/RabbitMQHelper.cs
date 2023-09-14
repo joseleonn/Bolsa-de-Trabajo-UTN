593eb29ca44f15722908491e50c1b779c3996b3a
jose caceres <josecaceresmusso@gmail.com>
2023-09-14 10:54:17 -0300

    admin changes

---
+++ Back_end/Src/BolsaDeTrabajo.Service/Helpers/RabbitMQHelper.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using Newtonsoft.Json;
+
+namespace BolsaDeTrabajo.Service.Helpers
+{
+    public class RabbitMQHelper
+    {
+        private readonly RabbitMQ.Client.IModel _channel;
+
+        public RabbitMQHelper()
+        {
+            // Crear la conexión y el canal a RabbitMQ (puedes hacerlo en el constructor o en otro lugar apropiado)
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost", // O la dirección IP de tu servidor RabbitMQ
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            var connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+        }
+
+        public void SendRabbitMessage(string destinatario, string asunto, string mensaje)
+        {
+            // Crear un objeto que represente el mensaje en un formato estructurado
+            var mensajeJson = new
+            {
+                Destinatario = destinatario,
+                Asunto = asunto,
+                Contenido = mensaje
+            };
+
+            // Serializar el objeto a una cadena JSON
+            var mensajeJsonString = JsonConvert.SerializeObject(mensajeJson);
+
+            // Convertir la cadena JSON en bytes UTF-8
+            var body = Encoding.UTF8.GetBytes(mensajeJsonString);
+
+
+            // Configurar las propiedades del mensaje
+
+            var properties = _channel.CreateBasicProperties();
+            properties.ContentType = "text/plain";
+
+            // Publicar el mensaje en la cola
+
+            _channel.BasicPublish(exchange: "",
+                                  routingKey: "EmailQueue",
+                                  basicProperties: properties,
+                                  body: body);
+        }
+    }
+}
