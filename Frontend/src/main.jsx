0227163d86c72dd72094504c33629800852787ec
FacuQuevedo <106189553+FacuQuevedo@users.noreply.github.com>
2023-09-22 15:02:47 -0300

    Provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { Providers } from "./pages/provider.jsx";
 // Define tu tema oscuro
 const darkTheme = {
   type: "dark",
@@ -19,11 +20,11 @@ const darkTheme = {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <NextUIProvider theme={darkTheme}>
+      <Providers>
         <main className="dark text-foreground bg-background">
           <App />
         </main>
-      </NextUIProvider>
+      </Providers>
     </Router>
   </React.StrictMode>
 );
